6b5ed846138e9734f051c2c0e348fe9a8805aa3e
brandosha <brandon.lantz@icloud.com>
2026-05-21 17:16:33 -0400

    Persist jwt signing key

--- config.example.ts
+++ config.example.ts
@@ -1,9 +1,4 @@
 export const config = {
   port: 7654,
   host: "localhost",
-  trello: {
-    apiKey: "",
-    secret: "",
-    token: "",
-  },
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,40 @@
-import { SignJWT, jwtVerify, generateKeyPair, JWTPayload } from "jose";
+import fs from "fs/promises";
+
+import { configDir, mkConfigDir } from "./paths.js";
+
+import { SignJWT, jwtVerify, generateSecret, JWTPayload, exportJWK, importJWK } from "jose";
 
 interface AuthPayload extends JWTPayload {
   email: string;
 }
 
-const keypair = generateKeyPair("Ed25519");
+const JWT_ALG = "HS256";
+
+const symmetricKey = (async () => {
+  await mkConfigDir;
+  const keyPath = `${configDir}/jwt_secret.json`;
+
+  try {
+    const raw = await fs.readFile(keyPath, "utf-8");
+    const jwk = JSON.parse(raw);
+    return await importJWK(jwk, JWT_ALG);
+  } catch (err: any) {
+    if (err?.code === "ENOENT") {
+      const newKey = await generateSecret(JWT_ALG, {
+        extractable: true,
+      }) as CryptoKey;
+      const jwk = await exportJWK(newKey);
+      await fs.writeFile(keyPath, JSON.stringify(jwk));
+      return newKey;
+    }
+    throw err;
+  }
+})();
 
 export async function generateAuthToken(payload: AuthPayload, expiresIn: string = "2w") {
-  const { privateKey } = await keypair;
+  const privateKey = await symmetricKey;
 
-  const alg = "EdDSA";
+  const alg = JWT_ALG;
   const jwt = await new SignJWT(payload)
     .setProtectedHeader({ alg })
     .setExpirationTime(expiresIn)
@@ -19,7 +44,7 @@ export async function generateAuthToken(payload: AuthPayload, expiresIn: string 
 }
 
 export async function verifyAuthToken(token: string) {
-  const { publicKey } = await keypair;
-  const jwt = await jwtVerify(token, publicKey);
+  const privateKey = await symmetricKey;
+  const jwt = await jwtVerify(token, privateKey);
   return jwt.payload;
 }
